5d3198ffea65dd06ed76d540fcbace3f393c0539
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-02-05 13:42:26 +0100

    Lesson 1 sprint 5 login with LocalStorage

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,25 +1,23 @@
 import axios from "axios"
-
-export const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
-    "API-KEY": import.meta.env.VITE_API_KEY,
-  },
-})
+import { AUTH_TOKEN } from "@/common/constants"
 
 // export const instance = axios.create({
 //   baseURL: import.meta.env.VITE_BASE_URL,
 //   headers: {
-//     // Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+//     Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
 //     "API-KEY": import.meta.env.VITE_API_KEY,
 //   },
 // })
-//
-// instance.interceptors.request.use(function (config) {
-//   const authToken = localStorage.getItem("auth-token")
-//   if (authToken) {
-//     config.headers.Authorization = `Bearer ${authToken}`
-//   }
-//   return config
-// })
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    // Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    "API-KEY": import.meta.env.VITE_API_KEY,
+  },
+})
+
+instance.interceptors.request.use(function (config) {
+  config.headers["Authorization"] = `Bearer ${localStorage.getItem(AUTH_TOKEN)}`
+  return config
+})
--- src/features/auth/model/auth-slice.ts
+++ src/features/auth/model/auth-slice.ts
@@ -5,6 +5,7 @@ import { ResaultCode } from "@/common/enums"
 import { handleServerAppError } from "@/common/utils/handleServerAppError.ts"
 import { handleServerNetworkError } from "@/common/utils/handleServerNetworkError.ts"
 import { authApi } from "@/features/auth/api/authApi.ts"
+import { AUTH_TOKEN } from "@/common/constants"
 
 export const authSlice = createAppSlice({
   name: "auth",
@@ -22,7 +23,7 @@ export const authSlice = createAppSlice({
           const res = await authApi.login(data)
           if (res.data.resultCode === ResaultCode.Success) {
             dispatch(setAppStatusAC({ status: "succeeded" }))
-            // localStorage.setItem(AUTH_TOKEN, res.data.data.token)
+            localStorage.setItem(AUTH_TOKEN, res.data.data.token)
             return { isLoggedIn: true }
           } else {
             handleServerAppError(res.data, dispatch)
