fbf58f38d40cd456d717c58a00794961a14c000f
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-02-05 21:56:26 +0100

    Lesson 1 sprint 5 loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css"
+
 import { Header } from "@/common/components/Header/Header"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
@@ -7,21 +7,34 @@ import { ThemeProvider } from "@mui/material/styles"
 import { selectThemeMode } from "@/app/app-slice.ts"
 import { ErrorSnackbar } from "@/common/components"
 import { Routing } from "@/common/routing"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { meTC } from "@/features/auth/model/auth-slice.ts"
+import { CircularProgress } from "@mui/material"
+import s from "./App.module.css"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
   const theme = getTheme(themeMode)
   const dispatch = useAppDispatch()
 
+  const [isInit, setIsInit] = useState(false)
+
   useEffect(() => {
-    dispatch(meTC())
+    dispatch(meTC()).finally(()=>{
+      setIsInit(true)
+    })
   }, [])
 
+  if(!isInit) {
+    return (
+      <div className={s.circularProgressContainer}>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
+      <div className={s.app}>
         <CssBaseline />
         <Header />
         <Routing />
