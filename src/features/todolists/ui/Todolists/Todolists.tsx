2ff7e6bb97be571fd880e4318281d16e37b5da18
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-02-10 14:01:28 +0100

    Lesson 2 sprint 5 Conditional fetching wuth use state

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,19 +2,23 @@ import { TodolistItem } from "./TodolistItem/TodolistItem"
 import Grid from "@mui/material/Grid2"
 import Paper from "@mui/material/Paper"
 import { useGetTodolistsQuery } from "@/features/todolists/api/todolistsApi.ts"
+import Button from "@mui/material/Button"
+import { useState } from "react"
 
 export const Todolists = () => {
-  // const todolists = useAppSelector(selectTodolists)
-  // const dispatch = useAppDispatch()
-  // useEffect(()=>{
-  //   dispatch(fetchTodoistsTC())
-  // },[])
 
-  const { data: todolists } = useGetTodolistsQuery()
-  // const res = useGetTodolistsQuery()
-  // console.log(res.da)
+  const [skip, setSkip] = useState(true)
+
+  const { data: todolists } = useGetTodolistsQuery(undefined, {skip})
+
+  const getTodosHandler = ()=>{
+    setSkip(false)
+  }
   return (
     <>
+      <div>
+        <Button onClick={getTodosHandler} >Get to do Lists</Button>
+      </div>
       {todolists?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
