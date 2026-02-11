5ff43891765249b8d396034f0d4d1ba66a0aadb7
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-02-11 15:41:55 +0100

    Lesson 2 sprint 5 Make authQuery

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,16 +1,39 @@
 import { LoginInputs } from "@/features/auth/lib/schemas/LoginSchema.ts"
 import { BaseResponse } from "@/common/types"
 import { instance } from "@/common/instance"
+import { baseApi } from "@/app/baseApi.ts"
 
-
-export const authApi = {
-  login(payload: LoginInputs){
-    return instance.post<BaseResponse<{userId:number; token:string}>>("/auth/login", payload)
+export const _authApi = {
+  login(payload: LoginInputs) {
+    return instance.post<BaseResponse<{ userId: number; token: string }>>("/auth/login", payload)
   },
-  logout(){
+  logout() {
     return instance.delete<BaseResponse>("/auth/login")
   },
-  me(){
-    return instance.get<BaseResponse<{id:number, email:string, login:string}>>("/auth/me")
-  }
+  me() {
+    return instance.get<BaseResponse<{ id: number; email: string; login: string }>>("/auth/me")
+  },
 }
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginInputs>({
+      query: (body) => ({
+        url: "/auth/login",
+        method: "post",
+        body,
+      }),
+    }),
+    logout: build.mutation<BaseResponse, void>({
+      query: () => ({
+        url: "/auth/login",
+        method: "delete",
+      }),
+    }),
+    me: build.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
+      query: () => ({
+        url: `/auth/me`,
+      }),
+    }),
+  }),
+})
